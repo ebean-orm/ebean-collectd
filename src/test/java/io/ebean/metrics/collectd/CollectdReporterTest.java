c255d2754617ae7f8f765b75f51140bc4861a8a9
rob bygrave <robin.bygrave@gmail.com>
2018-09-28 00:53:46 +1200

    No change - format only

--- src/test/java/io/ebean/metrics/collectd/CollectdReporterTest.java
+++ src/test/java/io/ebean/metrics/collectd/CollectdReporterTest.java
@@ -9,7 +9,7 @@ public class CollectdReporterTest {
 //    @ClassRule
 //    public static Receiver receiver = new Receiver(25826);
 
-    private CollectdReporter reporter;
+  private CollectdReporter reporter;
 
 //    @Before
 //    public void setUp() {
@@ -18,17 +18,17 @@ public class CollectdReporterTest {
 //                .build(new Sender("localhost", 25826));
 //    }
 
-    @Test
-    public void reportsByteGauges() throws Exception {
+  @Test
+  public void reportsByteGauges() throws Exception {
 
-        reporter = CollectdReporter.forRegistry(Ebean.getDefaultServer())
-          .withHostName("foobar")
-          .withDestHostName("localhost")
-          .withDestPort(25826)
-          .build();
+    reporter = CollectdReporter.forRegistry(Ebean.getDefaultServer())
+      .withHostName("foobar")
+      .withDestHostName("localhost")
+      .withDestPort(25826)
+      .build();
 
-        reporter.report(60);
-    }
+    reporter.report(60);
+  }
 
 
 }
--- src/test/java/io/ebean/metrics/collectd/Receiver.java
+++ src/test/java/io/ebean/metrics/collectd/Receiver.java
@@ -12,44 +12,44 @@ import java.util.concurrent.TimeUnit;
 
 public final class Receiver extends ExternalResource {
 
-    private final int port;
+  private final int port;
 
-    private UdpReceiver receiver;
-    private BlockingQueue<ValueList> queue = new LinkedBlockingQueue<>();
+  private UdpReceiver receiver;
+  private BlockingQueue<ValueList> queue = new LinkedBlockingQueue<>();
 
-    public Receiver(int port) {
-        this.port = port;
-    }
+  public Receiver(int port) {
+    this.port = port;
+  }
 
-    @Override
-    protected void before() throws Throwable {
-        receiver = new UdpReceiver(new Dispatcher() {
-            @Override
-            public void dispatch(ValueList values) {
-                queue.offer(new ValueList(values));
-            }
+  @Override
+  protected void before() throws Throwable {
+    receiver = new UdpReceiver(new Dispatcher() {
+      @Override
+      public void dispatch(ValueList values) {
+        queue.offer(new ValueList(values));
+      }
 
-            @Override
-            public void dispatch(Notification notification) {
-                throw new UnsupportedOperationException();
-            }
-        });
-        receiver.setPort(port);
-        new Thread(() -> {
-            try {
-                receiver.listen();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
+      @Override
+      public void dispatch(Notification notification) {
+        throw new UnsupportedOperationException();
+      }
+    });
+    receiver.setPort(port);
+    new Thread(() -> {
+      try {
+        receiver.listen();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }).start();
+  }
 
-    public ValueList next() throws InterruptedException {
-        return queue.poll(1, TimeUnit.SECONDS);
-    }
+  public ValueList next() throws InterruptedException {
+    return queue.poll(1, TimeUnit.SECONDS);
+  }
 
-    @Override
-    protected void after() {
-        receiver.shutdown();
-    }
+  @Override
+  protected void after() {
+    receiver.shutdown();
+  }
 }
